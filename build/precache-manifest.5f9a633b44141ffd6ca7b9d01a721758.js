2af9c07e0a52d4dbf9ca260f015dfa135c82968a
deepanshuRustagi <deepanshu@bigdrop.io>
2018-12-19 15:35:26 +0530

    build folder added

---
+++ build/precache-manifest.5f9a633b44141ffd6ca7b9d01a721758.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8476997c217a30ba354",
+    "url": "/static/css/main.74ddfb5a.chunk.css"
+  },
+  {
+    "revision": "a8476997c217a30ba354",
+    "url": "/static/js/main.a8476997.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "154b0727e1a82ec67a42a30dbc60afad",
+    "url": "/index.html"
+  }
+];
