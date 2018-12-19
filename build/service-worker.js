5b36f527039657960634caf3146fee4f0be8fa9a
Pratyum <pratyum@panalyt.com>
2018-12-19 19:04:44 +0800

    Added pictures + installed gh-pages to make the site work

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5f9a633b44141ffd6ca7b9d01a721758.js"
+  "haymarket.bmvhr.com/precache-manifest.b0b9ffbbff30fe4df35178644d916d69.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("haymarket.bmvhr.com/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
