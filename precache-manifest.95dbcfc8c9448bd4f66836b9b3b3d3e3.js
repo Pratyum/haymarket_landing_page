fd510c5b528140a529b1cabf97ea25910720817b
Pratyum <pratyum@panalyt.com>
2019-03-29 00:38:35 +0800

    Updates

---
+++ precache-manifest.95dbcfc8c9448bd4f66836b9b3b3d3e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "40f3ae705c2b5ea2daf1",
+    "url": "/static/js/main.40f3ae70.chunk.js"
+  },
+  {
+    "revision": "a633c38a8f8d90b2446d",
+    "url": "/static/js/1.a633c38a.chunk.js"
+  },
+  {
+    "revision": "40f3ae705c2b5ea2daf1",
+    "url": "/static/css/main.64d9d1a3.chunk.css"
+  },
+  {
+    "revision": "eb20ec3a44e1d2005e0468699d1e9477",
+    "url": "/index.html"
+  }
+];
