3dc831bac564cf8b42b4e4a168af7e0357ce3d31
Pratyum <pratyum@panalyt.com>
2019-04-02 19:33:00 +0800

    Updates

--- precache-manifest.09b1449f969a666545e2f66ab9c05ae1.js
+++ precache-manifest.09b1449f969a666545e2f66ab9c05ae1.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "fc019105543e903adc22",
-    "url": "/static/js/main.fc019105.chunk.js"
+    "revision": "2144f16683eeb9f31467",
+    "url": "/static/js/main.2144f166.chunk.js"
   },
   {
     "revision": "a633c38a8f8d90b2446d",
     "url": "/static/js/1.a633c38a.chunk.js"
   },
   {
-    "revision": "fc019105543e903adc22",
+    "revision": "2144f16683eeb9f31467",
     "url": "/static/css/main.3db49b1e.chunk.css"
   },
   {
-    "revision": "39a46fc122c24ab8e7ca5d8c5f5bd70d",
+    "revision": "f61acc4a237fbb61cfc511ac759fa4aa",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0303cdab9bb61f4ab15478382cf9aa3c.js"
+  "/precache-manifest.09b1449f969a666545e2f66ab9c05ae1.js"
 );
 
 workbox.clientsClaim();
